c970bbbc4c158952dbd3f678cc6543b1c6ae1d46
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2023-08-21 18:37:13 +0530

    Add files via upload

---
+++ springcore/src/main/java/spring/SpringCore/App.java
@@ -0,0 +1,13 @@
+package spring.SpringCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

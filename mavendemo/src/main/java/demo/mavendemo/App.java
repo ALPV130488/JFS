035de50b95bdf9b78782c657bcec730bba3b8358
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2023-08-21 13:43:34 +0530

    Add files via upload

---
+++ mavendemo/src/main/java/demo/mavendemo/App.java
@@ -0,0 +1,16 @@
+package demo.mavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public App() {
+		// TODO Auto-generated constructor stub
+	}
+}
